2cb1501790932fc532dad24ef0c200f7a1c35e79
mishraji.aaryan <mishraji.aaryan@gmail.com>
2025-01-24 08:35:03 +0000

    Append the config.py file

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
 
+#keep this below imports else will Throw Error
 app = Flask(__name__)
+
+#keep this after app = Flask(__name__) else will Throw Error
+import config
 
 @app.route('/')
 def index():
-    return render_template('index.html', var1="hello")
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
