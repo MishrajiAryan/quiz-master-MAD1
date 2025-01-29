6a184553609ecc53e1ea7ce4fc0ede55b7c6c2f1
Aryan Mishra <21f2000307@ds.study.iitm.ac.in>
2025-01-29 18:48:35 +0530

    Added register post method

--- models.py
+++ models.py
@@ -13,7 +13,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(256), nullable=False)  # Store hashed passwords
-    full_name = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(100), nullable=False)
     qualification = db.Column(db.String(100))
     dob = db.Column(db.Date)
 
--- routes.py
+++ routes.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
 from app import app
+from models import db, User, Subject, Chapter, Quiz, Question, Score
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
 
 #the decorator @app.route is directing towards root dir '/'
 @app.route('/')
@@ -13,3 +16,33 @@ def login():
 @app.route('/register')
 def register():
     return render_template('register.html')
+
+@app.route('/register', methods={'POST'})
+def register_post():
+    email= request.form.get('email')
+    password = request.form.get('password')
+    confirm_password = request.form.get('confirm_password')
+    name = request.form.get('name')
+    qualification = request.form.get('qualification')
+    dob = request.form.get('dob')
+
+    if not email or not password or not confirm_password or not name or not qualification or not dob:
+        flash('Please fill all the fields')
+        return redirect(url_for('register'))
+    
+    if password != confirm_password:
+        flash('Passwords do not match')
+        return redirect(url_for('register'))
+    
+    user = User.query.filter_by(email=email).first() #to check for unique prospect
+
+    if user:
+        flash('Email already exists')
+        return redirect(url_for('register'))
+
+    dob_date = datetime.strptime(dob, "%Y-%m-%d").date()
+    password_hash = generate_password_hash(password)
+    new_user = User(email=email, password=password_hash, name=name,qualification=qualification, dob=dob_date)
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect(url_for('login'))
