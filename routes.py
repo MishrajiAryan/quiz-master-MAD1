5bf2c27a6867b7ae8cd64e082936ddf2e527333a
Aryan Mishra <21f2000307@ds.study.iitm.ac.in>
2025-02-16 13:05:12 +0530

    updated routes

--- routes.py
+++ routes.py
@@ -1,6 +1,5 @@
 from app import app
 
-from api import api_bp
 from api.admin import admin_bp
 from api.auth import auth_bp
 from api.chapter import chapter_bp
@@ -9,12 +8,11 @@ from api.quiz import quiz_bp
 from api.subject import subject_bp
 from api.user import user_bp
 
-# API blueprint
-app.register_blueprint(api_bp, url_prefix="/api")
-app.register_blueprint(admin_bp, url_prefix="/api/admin")
-app.register_blueprint(auth_bp, url_prefix="/api/auth")
-app.register_blueprint(chapter_bp, url_prefix="/api/chapter")
-app.register_blueprint(question_bp, url_prefix="/api/question")
-app.register_blueprint(quiz_bp, url_prefix="/api/quiz")
-app.register_blueprint(subject_bp, url_prefix="/api/subject")
-app.register_blueprint(user_bp, url_prefix="/api/user")
+# Register individual blueprints without the /api prefix
+app.register_blueprint(admin_bp, url_prefix="/admin")
+app.register_blueprint(auth_bp, url_prefix="/")
+app.register_blueprint(chapter_bp, url_prefix="/chapter")
+app.register_blueprint(question_bp, url_prefix="/question")
+app.register_blueprint(quiz_bp, url_prefix="/quiz")
+app.register_blueprint(subject_bp, url_prefix="/subject")
+app.register_blueprint(user_bp, url_prefix="/user")
